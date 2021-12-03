66d477a94b7c3a61911245d70a3a062cdb8d9287
aymenmelki <79868919+aymenmelki@users.noreply.github.com>
2021-12-03 16:16:06 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,39 +1,16 @@
-﻿using System.Linq;
 using System.Net.Http.Headers;
 
 
 //   APIUrl = "https://bitbucket.org/aymenmelki/test/src/main/FileUser.txt";
+    var client = new HttpClient();
 
-
-//string text = System.IO.File.ReadAllText(@"C:/Users/21658/source/repos/ConsoleApp1/ConsoleApp1/aymen.txt");
-string text = System.IO.File.ReadAllText(@"C:/Users/21658/source/repos/ConsoleApp1/ConsoleApp1/aymen.txt");
-
-
-string[] lines = System.IO.File.ReadAllLines(@"C:/Users/21658/source/repos/ConsoleApp1/ConsoleApp1/aymen.txt");
-foreach (string line in lines)
-{
-    string[] subs = line.Split(' ');
-
-    foreach (var sub in subs)
-
-    {
-        string[] sub12 = sub.Split(':');
-
-        Console.WriteLine(sub12[3]);
-
-
-
-    }
-    
-}
-
-var client = new HttpClient();
-
-    client.BaseAddress = new Uri("https://bitbucket.org/aymenmelki/apptest/src/main/Userfile.text");
+    client.BaseAddress = new Uri("https://api.bitbucket.org/2.0/users/alfasoftdev");
     client.DefaultRequestHeaders.Accept.Clear();
     client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-    HttpResponseMessage response = await client.GetAsync("https://bitbucket.org/aymenmelki/apptest/src/main/Userfile.text");
+    HttpResponseMessage response = await client.GetAsync("https://api.bitbucket.org/2.0/users/alfasoftdev");
     Console.WriteLine("response");
 var readTask = response.Content.ReadAsStringAsync().ConfigureAwait(false);
 var rawResponse = readTask.GetAwaiter().GetResult();
-//Console.WriteLine(rawResponse);
+Console.WriteLine(rawResponse);
+
+
